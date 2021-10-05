e3d16051c895325ba5fef19ffb7e327e09293c03
Lasse Hyldahl Jensen <lasse@lasse-it.dk>
2021-10-05 10:40:24 +0200

    Added a PRISM template project

---
+++ ex1_the_debt_book/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ex1_the_debt_book.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace ex1_the_debt_book
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ ex1_the_debt_book/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ex1_the_debt_book.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
